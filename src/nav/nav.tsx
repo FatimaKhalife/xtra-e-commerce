de9feb23e97a8407bdaa7321fd8292e23e5a6352
Fatima Khalife <fatimakhalifekhalife@gmail.com>
2026-05-04 16:09:54 +0300

    again nav

--- src/nav/nav.tsx
+++ src/nav/nav.tsx
@@ -99,7 +99,7 @@ function Nav() {
               <li><Link to="/contact">Contact</Link></li>
 
               {logged ?
-                <li><a href="" onClick={handleLogout}>Logout</a></li>  :
+                <li onClick={handleLogout}><a href="">Logout</a></li>  :
                 <li><Link to="/login">Login</Link></li>
               }
               <li><a href="#" className="icon-link" onClick={() => setopen(true)}><GrSearch className="icon2" /></a></li>
