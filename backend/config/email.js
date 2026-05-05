c6bcee769bba00764cf0878b68967d0662cf5f02
Fatima Khalife <fatimakhalifekhalife@gmail.com>
2026-05-05 18:31:00 +0300

    stmp2

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,28 +1,31 @@
-import { Resend } from 'resend';
-
-export const sendMail = async ({ to, subject, text }) => {
-    await transporter.sendMail({
-        from: `"Xtra Shop" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        text
-    });
-};
 import nodemailer from "nodemailer";
 
+// TRANSPORTER
 export const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
-   port: 465,
-   secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
-transporter.verify((error, success) => {
+
+// VERIFY CONNECTION
+transporter.verify((error) => {
     if (error) {
         console.log("SMTP ERROR:", error);
     } else {
         console.log("SMTP READY");
     }
 });
+
+// SEND MAIL
+export const sendMail = async ({ to, subject, text }) => {
+    await transporter.sendMail({
+        from: `"Xtra Shop" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        text,
+    });
+};
