731875ae43700fed86b6037322843de912a5cded
Fatima Khalife <fatimakhalifekhalife@gmail.com>
2026-05-11 18:14:30 +0300

    partner responsive

--- src/partners/partners.tsx
+++ src/partners/partners.tsx
@@ -10,7 +10,7 @@ export default function Partners(){
                 <button className="partner-button">ALL PARTNERS</button>
             </div>
             <div className="partner-img">
-                <img className="partner-img1" src="https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/l6.png" alt="" />
+                <img src="https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/l6.png" alt="" />
                  <img src="https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/l5.png" alt="" />
                   <img src="https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/l4.png" alt="" />
                    <img src="https://xtratheme.com/elementor/factory/wp-content/uploads/sites/8/2018/09/l1.png" alt="" />
