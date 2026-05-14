6004c3f02f7b44210027f84351ec481608a8f196
Fatima Khalife <fatimakhalifekhalife@gmail.com>
2026-05-14 14:07:27 +0300

    url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,1 @@
-// export const API_URL = (import.meta.env.VITE_API_URL || "http://localhost:5000").replace(/\/$/, "");
-export const API_URL =
-  "https://xtra-e-commerce-production.up.railway.app";
+export const API_URL = (import.meta.env.VITE_API_URL || "http://localhost:5000").replace(/\/$/, "");
